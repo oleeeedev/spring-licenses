6f904a1c1baec67f48c85c2edc56510969bcbe69
oleeeedev <oleschumacherr1801@gmail.com>
2024-12-31 14:07:54 +0100

    Add LicensesApplication entry point for Spring Boot app
    Introduced the main application class with the @SpringBootApplication annotation. This serves as the entry point for the Spring Boot application, enabling it to run using SpringApplication.

---
+++ src/main/java/dev/ole/spring/LicensesApplication.java
@@ -0,0 +1,12 @@
+package dev.ole.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicensesApplication.class, args);
+    }
+}
